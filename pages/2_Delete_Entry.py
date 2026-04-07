b7d43e8370ffe3e74de2c159b3bf1c56488882c6
mrcneumeyer-collab <mrcneumeyer@gmail.com>
2026-04-07 15:29:31 -0700

    Update 2_Delete_Entry.py

--- pages/2_Delete_Entry.py
+++ pages/2_Delete_Entry.py
@@ -4,7 +4,7 @@ import psycopg2
 st.set_page_config(page_title="Delete Food Entry", page_icon="🗑️")
 
 def get_connection():
-    return psycopg2.connect(st.secrets["DB_URL"])
+    return psycopg2.connect(st.secrets["DB_URL1"])
 
 st.title("🗑️ Delete a Food Entry")
 
